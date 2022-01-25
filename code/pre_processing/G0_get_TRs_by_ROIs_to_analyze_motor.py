8ac58554649669bde7aecd588a8afec3a993a506
jennwilliams <jlw1@cs.cmu.edu>
2022-01-25 11:52:26 -0500

    Add code to get TRs by ROIs motor task

---
+++ code/pre_processing/G0_get_TRs_by_ROIs_to_analyze_motor.py
@@ -0,0 +1,55 @@
+import os 
+import pandas as pd
+import numpy as np 
+import scipy as sp
+from pre_processing_utils import read_json_list, check_subtask_consistency, get_timing_RL, get_timing_LR, get_motor_task_cue_timing, get_motor_task_cue_timing_from_evs
+
+def subset_train_test_data_motor_tr_by_roi_data(motor_timing, subject_list, run, zscore = True):
+    train_subjects_tr_by_roi = {}
+    test_subjects_tr_by_roi = {}
+    for sub_idx, sub in enumerate(subject_list):
+    
+        f = "../../data/HCP_3T_Task/pre_processed/{subject}_tfMRI_{run_name}_shen268_roi_ts.txt".format(subject = sub,
+                                                                                            run_name = run)
+        if os.path.isfile(f):
+            run_data = pd.read_csv(f, sep='\t', header=None).dropna(axis=1)
+            start_train = np.ceil(motor_timing[sub_idx, 5] / .72 ) # .72 is TR length https://www.mail-archive.com/hcp-users@humanconnectome.org/msg00616.html
+            start_test = np.ceil((motor_timing[sub_idx, 7]) / .72)
+            stop_train = start_test - 15  # subtract 15 seconds based off canonical HRF
+            
+            stop_test = run_data.shape[0]
+            train_subjects_tr_by_roi[sub] = run_data.iloc[int(start_train):int(stop_train),:].reset_index(drop = True)
+            test_subjects_tr_by_roi[sub] = run_data.iloc[int(start_test):int(stop_test),:].reset_index(drop = True)
+            
+            if zscore: 
+                train_subjects_tr_by_roi[sub] = sp.stats.zscore(train_subjects_tr_by_roi[sub])
+                test_subjects_tr_by_roi[sub] = sp.stats.zscore(test_subjects_tr_by_roi[sub])
+                
+    return train_subjects_tr_by_roi, test_subjects_tr_by_roi
+    
+
+# Check motor stimulus subtasks consistent across participants
+check_subtask_consistency(182, 117, "tfMRI_MOTOR_RL")
+check_subtask_consistency(182, 117, "tfMRI_MOTOR_LR")
+
+# Get timing of motor stimulus subtasks, and which participants don't have motor fMRI data or a descriptive tab file
+train_subjects = read_json_list(train_subjects_list) #contact us for access to train_subjects_list (the list of participants we have selected for the development set)
+motor_rl_timing, sub_no_rl_motor_data, sub_no_rl_tab_file = get_motor_task_cue_timing(train_subjects, "tfMRI_MOTOR_RL", "lf") 
+motor_lr_timing, sub_no_lr_motor_data, sub_no_lr_tab_file = get_motor_task_cue_timing(train_subjects, "tfMRI_MOTOR_LR", "rf") 
+
+# For participants without a descriptive tab file, need to get the timing from their HCP EV file
+motor_rl_timing_complete = get_motor_task_cue_timing_from_evs(motor_rl_timing, sub_no_rl_tab_file, train_subjects, "tfMRI_MOTOR_RL", "lf") 
+motor_lr_timing_complete = get_motor_task_cue_timing_from_evs(motor_lr_timing, sub_no_lr_tab_file, train_subjects, "tfMRI_MOTOR_LR", "rf") 
+
+# Split each participant's motor run into train and test: use first 1/2 to train and second 1/2 to test 
+# Subset ROI data
+train_rl, test_rl = subset_train_test_data_motor_tr_by_roi_data(motor_rl_timing_complete, 
+                                                train_subjects, "MOTOR_RL", zscore = True)
+test_lr, train_lr = subset_train_test_data_motor_tr_by_roi_data(motor_lr_timing_complete, 
+                                                train_subjects, "MOTOR_LR", zscore = True)
+
+# Save 
+save_dict(train_rl, "../../data/subjwise_dict_TR_by_ROI_matrix_zscored_per_participant_MOTOR_LR_train")
+save_dict(train_lr, "../../data/subjwise_dict_TR_by_ROI_matrix_zscored_per_participant_MOTOR_LR_train")
+save_dict(test_rl, "../../data/subjwise_dict_TR_by_ROI_matrix_zscored_per_participant_MOTOR_RL_test")
+save_dict(test_lr, "../../data/subjwise_dict_TR_by_ROI_matrix_zscored_per_participant_MOTOR_LR_test")
--- code/pre_processing/pre_processing_utils.py
+++ code/pre_processing/pre_processing_utils.py
@@ -3,6 +3,10 @@ import pandas as pd
 import numpy as np 
 import json
 import scipy as sp
+import nibabel as nib 
+from glob import glob
+import fnmatch
+import os
 
 run_name_dict = {
     "REST1": "REST1_7T_PA",
@@ -27,7 +31,7 @@ def mk_TR_by_Feature(clip, feature_file):
 
 def get_visual_semantic_dropped_TRs(clip):
     run_name =  run_name_dict[clip]
-    hf = h5py.File('../data/7T_movie_resources/WordNetFeatures.hdf5', 'r')
+    hf = h5py.File('../../data/7T_movie_resources/WordNetFeatures.hdf5', 'r')
     clip_features = hf.get(run_name)
     clip_features = np.array(clip_features)
 
@@ -56,9 +60,9 @@ def read_json_list(fileName):
         b = json.load(fp)
     return b
 
-def get_TRs_to_analyze(clip, 
+def get_TRs_with_visual_features(clip, 
                        start_stop_pads = (10, 5),
-                       subj_list="train_subjects_list.npy"): # Contact us for access to train_subjects_list 
+                       subj_list="../../data/train_subjects_list.npy"): # Contact us for access to train_subjects_list 
                         
     subj_list = np.load(subj_list, allow_pickle=True)
     
@@ -67,7 +71,7 @@ def get_TRs_to_analyze(clip,
 
     if 'MOVIE' in clip:
         print("Removing TRs where no visual semantic features")
-        hf = h5py.File('../data/WordNetFeatures.hdf5', 'r') # File from HCP data from gallant lab's featurizations
+        hf = h5py.File('../../data/WordNetFeatures.hdf5', 'r') # File from HCP data from gallant lab's featurizations
         estIdx = hf.get(run_name + "_est")[()]
         valIdx = hf.get(run_name + "_val")[()]
         
@@ -80,7 +84,7 @@ def get_TRs_to_analyze(clip,
 
 def make_TR_by_ROI_dict(clip, 
                         start_stop_pads = (10, 5),
-                        subj_list="train_subjects_list.npy"): # Contact us for access to train_subjects_list 
+                        subj_list="../../data/train_subjects_list.npy"): # Contact us for access to train_subjects_list 
                         
     subj_list = np.load(subj_list, allow_pickle=True)
     subjwise_ts_dict = {}
@@ -114,3 +118,140 @@ def make_TR_by_ROI_dict(clip,
         subjwise_ts_dict[subj] = sp.stats.zscore(run_data) 
 
     return subjwise_ts_dict
+
+def get_voxels_masked_zscored(nii_file_name, movie_idx, thin_mask):
+    nii = nib.load(nii_file_name)
+    nii_data = np.asanyarray(nii.dataobj).T
+    nii_data_idx = nii_data[movie_idx ,:]
+    nii_data_idx_masked = nii_data_idx[:, thin_mask]
+    niiData_zscored = sp.stats.zscore(nii_data_idx_masked, axis=0)
+    niiData_preprocessed = np.nan_to_num(niiData_zscored, nan=0.0)
+    return niiData_preprocessed
+
+
+# Check that all participants were presented the same subtasks in the same order in the motor stimulus 
+def check_subtask_consistency(num_participants, data_file_rows, task_name): #can use this function for other HCP 3T tasks
+    block_order_motor_task = pd.DataFrame(np.zeros((num_participants, data_file_rows)))
+    count = 0 
+    for d in glob("../../data/HCP_3T_Task/*"):
+        if d != "../../data/HCP_3T_Task/pre_processed":
+            for file in os.listdir(d + "../../data/MNINonLinear/Results/" + task_name + "/"):
+                if fnmatch.fnmatch(file, "*_TAB.txt"):
+                    data_file = pd.read_csv("../../data/HCP_3T_Task/" + d + "/MNINonLinear/Results/" + task_name + "/" + file, delimiter = "\t")
+                    block_order_motor_task.iloc[count, :] = data_file["BlockType"]
+                    count += 1
+                
+    ## check if any columns (subtasks) differ
+    subset_block_order_motor_task = block_order_motor_task.iloc[0:count,:]
+    if subset_block_order_motor_task[subset_block_order_motor_task.columns[subset_block_order_motor_task.apply(lambda s: len(s.unique()) > 1)]].shape[1] > 0:
+        print("For at least 1 participant 1 of the subtasks differs from the other participants. Will need to further investigate the task to continue")
+    else:
+        print("Check complete, can proceed with pre-processing data")
+
+def get_timing_RL(timing, first_task, data_file):
+    start_time = "{}Cue.OnsetTime".format(first_task)
+    start_values = data_file[start_time].dropna().unique()
+
+    timing[sub_idx, 1] = start_values[0] 
+    timing[sub_idx, 3] = start_values[1]
+
+    trs_start_collecting = data_file["CountDownSlide.OnsetTime"] 
+    timing[sub_idx, 4] =  trs_start_collecting[0]
+    
+    # fixation time - 1st fixation time and 3rd fixation time 
+    fixation_onsets = data_file["Fixdot.OnsetTime"].dropna().unique()
+    timing[sub_idx, 8] = fixation_onsets[0]
+    timing[sub_idx, 9] = fixation_onsets[2]
+
+    return timing
+
+def get_timing_LR(timing, first_train_task, first_test_task, data_file):
+
+    start_time_train = "{}Cue.OnsetTime".format(first_train_task)
+    start_time_test = "{}Cue.OnsetTime".format(first_test_task)
+
+    start_values_train = data_file[start_time_train].dropna().unique()
+    start_values_test = data_file[start_time_test].dropna().unique()
+
+    timing[sub_idx, 1] = start_values_train[0] 
+    timing[sub_idx, 3] = start_values_test[1]
+
+    trs_start_collecting = data_file["CountDownSlide.OnsetTime"] 
+    timing[sub_idx, 4] =  trs_start_collecting[0]
+    
+    # fixation time - 2nd fixation time and 3rd fixation time 
+    fixation_onsets = data_file["Fixdot.OnsetTime"].dropna().unique()
+    timing[sub_idx, 8] = fixation_onsets[1]
+    timing[sub_idx, 9] = fixation_onsets[2]
+
+    return timing
+
+def get_motor_task_cue_timing(subject_list, run, last_task): # function for RL or LR encoding of fMRI scan 
+    timing = np.zeros((len(subject_list), 12))
+    sub_without_motor = list()
+    sub_no_tab_file = list()
+    for sub_idx, sub in enumerate(subject_list):
+        d = "../../data/HCP_3T_Task/{}".format(sub)
+        timing[sub_idx, 0] = sub
+        if os.path.isdir(d):
+            
+            for file in os.listdir("{dir_name}/MNINonLinear/Results/{run_name}/".format(dir_name = d, run_name = run)):
+                if fnmatch.fnmatch(file, "*_TAB.txt"):
+                    data_file = pd.read_csv("{dir_name}/MNINonLinear/Results/{run_name}/{file_name}".format(dir_name=d, 
+                                                                        run_name = run, file_name = file), delimiter = "\t")
+                    cue_idx = 0 
+                    if run contains "RL":
+                        timing = get_timing_RL(timing, "LeftHand", data_file)
+                
+                    elif run contains "LR":
+                         timing = get_timing_LR(timing, "RightHand", "Tongue", data_file)
+
+                else: # if tab file doesn't exist even though fMRI data exists 
+                    sub_no_tab_file.append(sub)
+                
+            if os.path.isfile("{dir_name}/MNINonLinear/Results/{run_name}/EVs/{last_task_name}.txt".format(dir_name = d, 
+                                                                run_name = run, last_task_name = last_task)): #ev file exists 
+                last_task_file = pd.read_csv("{dir_name}/MNINonLinear/Results/{run_name}/EVs/{last_task_name}.txt".format(dir_name = d, 
+                                                                 run_name = run, last_task_name = last_task), delimiter = "\t")
+                timing[sub_idx, 2] = last_task_file.iloc[-1, 0]
+            else: 
+                print("sub {} no ev file".format(sub))
+        else: 
+            sub_without_motor.append(sub)
+                    
+    # rescale timing: need to remove the countDownSlide.OnsetTime which is when TRs started being recorded to make 
+    # sure timing is in line with tr recording
+    timing[:, 5] = (timing[:, 1] - timing[:, 4]) / 1000  #timing was in milliseconds --> seconds
+    timing[:, 6] = (timing[:, 2] + 12)  # time was in seconds
+    timing[:, 7] = (timing[:, 3] - timing[:, 4]) / 1000  #timing was in milliseconds 
+    timing[:, 10] = (timing[:, 8] - timing[:, 4]) / 1000  #timing was in milliseconds 
+    timing[:, 11] = (timing[:, 9] - timing[:, 4]) / 1000  #timing was in milliseconds 
+
+    return timing, sub_without_motor, sub_no_tab_file 
+
+def get_motor_task_cue_timing_from_evs(timing_file, subjects_no_tab_file, all_subjects, run, last_task_name):
+    for sub in subjects_no_tab_file:
+        sub_idx = all_subjects.index(sub) # get from idx in sub list
+        d = "../../data/HCP_3T_Task/{}".format(sub)
+        if os.path.isdir(d):
+            cue_file = "{dir_name}/MNINonLinear/Results/{run_name}/EVs/cue.txt".format(dir_name = d, run_name = run)
+            last_task_file = "{dir_name}/MNINonLinear/Results/{run_name}/EVs/{short_task_name}.txt".format(dir_name = d, run_name = run, short_task_name = last_task_name)
+            if os.path.isfile(cue_file):
+                cue = pd.read_csv(cue_file, delimiter = "\t", header = None)
+                cue_times = cue.iloc[:, 0]
+                timing_file[sub_idx, 1] = cue_times.iloc[0] 
+                timing_file[sub_idx, 3] = cue_times.iloc[5]
+                
+            if os.path.isfile(last_task_file):
+                last_task = pd.read_csv(last_task_file, delimiter = "\t")
+                last_task_times = last_task.iloc[:, 0]
+                timing_file[sub_idx, 2] = last_task_times.iloc[-1]
+                
+        # timing in ev files is already 0 indexed - as in 0 is start of when TRs collected 
+        # to keep timing matrix consistent with matrix from get_motor_task_cue_timing function 
+        # just copied these times to later cols 
+        timing_file[sub_idx, 5] = timing_file[sub_idx, 1] 
+        timing_file[sub_idx, 6] = timing_file[sub_idx, 2] + 12 
+        timing_file[sub_idx, 7] = timing_file[sub_idx, 3] 
+
+    return timing_file
